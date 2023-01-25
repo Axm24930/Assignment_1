31af1647cae89fe25e36ef068575aa6f225f7178
Axm24930 <123587657+Axm24930@users.noreply.github.com>
2023-01-25 12:44:00 -0600

    Created Question_3.py

---
+++ QUestion_3.py
@@ -0,0 +1,14 @@
+#Tuple For Sisters
+Sisters=('Mythili','Mounika','Sharanya')
+#Tuple For Brothers
+Brothers=('Vishal','Shentan','Aditya','Sai')
+#Joining Tupled of Brothers and Sisters  and assigning it to siblings
+siblings=Sisters+Brothers
+print("Siblings:",siblings)
+#How many siblings do you have
+print("Number of Siblings:",len(siblings))
+#modifying siblings by adding mother and father
+siblings= siblings +('Shashikala','Sampath Rao')
+#Adding siblings and parents to family members
+family_members=siblings
+print("Family members :",family_members)
