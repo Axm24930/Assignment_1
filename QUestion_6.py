a8f2da2c8eb770d3c22e158fd2648fa0f5a660c1
Axm24930 <123587657+Axm24930@users.noreply.github.com>
2023-01-25 12:46:06 -0600

    Create QUestion_6.py

---
+++ QUestion_6.py
@@ -0,0 +1,5 @@
+str="I am a teacher and I love to inspire and teach people"
+_unique_words=set(str.split(" "))
+leng=len(_unique_words)
+print(_unique_words)
+print(" unique words have been used in the sentence are", leng)
